ab98972f12fb1b2767652fe2f33f5c97543d2142
MarcinBalejko <mbbm9@o2.pl>
2019-09-05 22:25:21 +0200

    :recycle: Refactored

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class BookmarkManager < Sinatra::Base
     end
 
     post '/bookmarks' do
-        Bookmark.create(url: params[:url])
+        Bookmark.create(url: params['url'])
         redirect '/bookmarks'
     end
 
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,18 +1,20 @@
 require 'pg'
 
 feature 'Viewing bookmarks' do
-  scenario 'A user can see bookmarks' do
+  scenario 'Visiting /bookmarks shows me all the bookmarks' do
+
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
     # Add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
 
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
+      
     visit('/bookmarks')
-
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
   end
+
 end
