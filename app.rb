f386273e4e498df0653f20296d686af7771ec9f3
MarcinBalejko <mbbm9@o2.pl>
2019-09-05 21:22:34 +0200

    :recycle: Refactored

--- app.rb
+++ app.rb
@@ -18,6 +18,11 @@ class BookmarkManager < Sinatra::Base
     post '/bookmarks' do
         p "Form data submitted to the /bookmarks route!"
         p params
+        url = params['url']
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+        connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+        redirect '/bookmarks'
+        
     end
 
     run! if app_file == $0
