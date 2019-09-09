565a36795fa75ba84176979e3c7cd1a7b2260503
MarcinBalejko <mbbm9@o2.pl>
2019-09-10 01:33:05 +0200

    :recycle: Refactored

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -12,7 +12,7 @@ class Bookmark
 
   def self.delete(id:)
 
-    
+
     DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id}")
   end
 
@@ -35,23 +35,15 @@ class Bookmark
   end
     
   def self.update(id:, title:, url:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    result = connection.exec("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+    
+    result = DatabaseConnection.query("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
   def self.find(id:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
+
     
-    result = connection.exec("SELECT * FROM bookmarks WHERE id = #{id}")
+    result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{id}")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,11 +1,15 @@
 require 'pg'
 
 class DatabaseConnection
-    def self.setup(dbname)
-        @connection = PG.connect(dbname: dbname)
-    end
+  def self.setup(dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
 
-    def self.query(sql)
-        @connection.exec(sql)
-    end
+  def self.connection
+    @connection
+  end
+
+  def self.query(sql)
+    @connection.exec(sql)
+  end
 end
