7e98d27e14e1d403ea6c45f80d6a6993ecc64626
MarcinBalejko <mbbm9@o2.pl>
2019-09-12 22:09:03 +0200

    :white_check_mark: Truncates all of the tables

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,5 @@ def setup_test_database
   connection = PG.connect(dbname: 'bookmark_manager_test')
 
   # Clean the bookmarks table
-  connection.exec("TRUNCATE bookmarks, comments;")
+  connection.exec("TRUNCATE bookmarks, comments, tags, bookmarks_tags;")
 end
