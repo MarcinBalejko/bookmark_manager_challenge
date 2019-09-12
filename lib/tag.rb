f42a9c373a5e87bbf80cc7dd47bde945bdc2a749
MarcinBalejko <mbbm9@o2.pl>
2019-09-12 23:05:15 +0200

    :white_check_mark: Tag classe's 'where' method  returns tags linked to the given bookmark id

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,14 +1,23 @@
 require_relative './database_connection'
 
 class Tag
-    attr_reader :id, :content
-    
-    def initialize(id:, content:)
-        @id = id
-        @content = content
+  attr_reader :id, :content
+
+  def initialize(id:, content:)
+    @id = id
+    @content = content
+
+  end
+
+  def self.create(content:)
+    result = DatabaseConnection.query("INSERT INTO tags (content) VALUES('#{content}') RETURNING id, content;")
+    Tag.new(id: result[0]['id'], content: result[0]['content'])
+  end
+
+  def self.where(bookmark_id:)
+    result = DatabaseConnection.query("SELECT id, content FROM bookmarks_tags INNER JOIN tags ON tags.id = bookmarks_tags.tag_id WHERE bookmarks_tags.bookmark_id = '#{bookmark_id}';")
+    result.map do |tag|
+      Tag.new(id: tag['id'], content: tag['content'])
     end
-    def self.create(content:)
-        result = DatabaseConnection.query("INSERT INTO tags (content) VALUES('#{content}') RETURNING id, content;")
-        Tag.new(id: result[0]['id'], content: result[0]['content'])
-    end
+  end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -14,4 +14,21 @@ describe Tag do
       expect(tag.content).to eq 'test tag'
     end
   end
+  describe '.where' do
+    it 'returns tags linked to the given bookmark id' do
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      tag1 = Tag.create(content: 'test tag 1')
+      tag2 = Tag.create(content: 'test tag 2')
+      BookmarkTag.create(bookmark_id: bookmark.id, tag_id: tag1.id)
+      BookmarkTag.create(bookmark_id: bookmark.id, tag_id: tag2.id)
+
+      tags = Tag.where(bookmark_id: bookmark.id)
+      tag = tags.first
+
+      expect(tags.length).to eq 2
+      expect(tag).to be_a Tag
+      expect(tag.id).to eq tag1.id
+      expect(tag.content).to eq tag1.content
+    end
+  end
 end
