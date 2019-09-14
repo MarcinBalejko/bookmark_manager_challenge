879f0af756c8ea01d02bfe3735175eb0115f440a
MarcinBalejko <mbbm9@o2.pl>
2019-09-14 22:24:47 +0200

    :construction: User class

--- app.rb
+++ app.rb
@@ -66,5 +66,14 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    User.create(email: params[:email], password: params[:password])
+    redirect '/bookmarks'
+  end
+
   run! if app_file == $0
 end
