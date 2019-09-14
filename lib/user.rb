dc969577aa5c79cb19df3d1a4891cd3e66776b16
MarcinBalejko <mbbm9@o2.pl>
2019-09-15 01:24:40 +0200

    :white_check_mark: Password encrypting

--- lib/user.rb
+++ lib/user.rb
@@ -1,19 +1,24 @@
-require 'database_connection'
+require 'bcrypt'
+require_relative './database_connection'
 
 class User
   def self.create(email:, password:)
-    result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{password}') RETURNING id, email;")
-    User.new(id: result[0]['id'], email: result[0]['email'])
-  end
-
-  def self.create(email:, password:)
-    # encrypt the plantext password
     encrypted_password = BCrypt::Password.create(password)
 
-    # insert the encrypted password into the database, instead of the plaintext one
-    result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{encrypted_password}') RETURNING id, email")
+    result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{encrypted_password}') RETURNING id, email;")
+    User.new(
+      id: result[0]['id'],
+      email: result[0]['email'],
+    )
+  end
 
-    User.new(id: result[0]['id'], email: result[0]['email'])
+  def self.find(id:)
+    return nil unless id
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = #{id}")
+    User.new(
+      id: result[0]['id'],
+      email: result[0]['email'],
+    )
   end
 
   attr_reader :id, :email
@@ -22,13 +27,6 @@ class User
     @id = id
     @email = email
   end
-
-  def self.find(id)
-    return nil unless id
-    result = DatabaseConnection.query("SELECT * FROM users WHERE id ='#{id}';")
-    User.new(result[0]['id'], result[0]['email'])
-  end
-
 end
 
 
