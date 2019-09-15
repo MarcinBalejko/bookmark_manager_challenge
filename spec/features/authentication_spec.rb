2619bb8c3fdf22f76f30246d78f220dcf3fd331e
MarcinBalejko <mbbm9@o2.pl>
2019-09-16 01:25:54 +0200

    :white_check_mark: Returns nil given incorrect password

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -11,13 +11,24 @@ feature 'Authentication' do
 
     expect(page).to have_content 'Welcome, test@example.com'
   end
-
   scenario 'a user sees an error if they get their email wrong' do
     User.create(email: 'test@example.com', password: 'password123')
 
     visit '/sessions/new'
     fill_in(:email, with: 'nottherightemail@me.com')
     fill_in(:password, with: 'password123')
+    click_button('Sign in')
+
+    expect(page).not_to have_content 'Welcome, test@example.com'
+    expect(page).to have_content 'Please check your email or password.'
+  end
+
+  scenario 'a user sees an error if they get their password wrong' do
+    User.create(email: 'test@example.com', password: 'password123')
+
+    visit '/sessions/new'
+    fill_in(:email, with: 'test@example.com')
+    fill_in(:password, with: 'wrongpassword')
     click_button('Sign in')
 
     expect(page).not_to have_content 'Welcome, test@example.com'
