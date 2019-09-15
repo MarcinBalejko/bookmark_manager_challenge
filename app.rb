9c5eb6c494ab0099831226df3dcead32b57914af
MarcinBalejko <mbbm9@o2.pl>
2019-09-16 01:03:33 +0200

    :white_check_mark: Returns nil given an incorrect email address

--- app.rb
+++ app.rb
@@ -84,8 +84,14 @@ class BookmarkManager < Sinatra::Base
 
   post '/sessions' do
     user = User.authenticate(email: params[:email], password: params[:password])
-    session[:user_id] = user.id
-    redirect('/bookmarks')
+  
+    if user
+      session[:user_id] = user.id
+      redirect('/bookmarks')
+    else
+      flash[:notice] = 'Please check your email or password.'
+      redirect('/sessions/new')
+    end
   end
 
 
