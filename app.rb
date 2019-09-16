1dca5441464a20addc099f5f40395d08f7bd6389
MarcinBalejko <mbbm9@o2.pl>
2019-09-16 02:04:02 +0200

    :sparkle: :white_check_mark: A user can sign out

--- app.rb
+++ app.rb
@@ -94,6 +94,12 @@ class BookmarkManager < Sinatra::Base
     end
   end
 
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect('/bookmarks')
+  end
+
 
 
   run! if app_file == $0
