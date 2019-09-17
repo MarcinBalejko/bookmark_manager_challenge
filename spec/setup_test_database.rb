7629712440f1a9f1a923fd2b431ce3c46b14ac77
MarcinBalejko <mbbm9@o2.pl>
2019-09-17 02:31:12 +0200

    :fire: setup_test_database deleted

--- spec/setup_test_database.rb
+++
@@ -1,7 +0,0 @@
-require 'pg'
-def setup_test_database
-  connection = PG.connect(dbname: 'bookmark_manager_test')
-
-  # Clean the bookmarks table
-  connection.exec("TRUNCATE bookmarks, comments, tags, bookmarks_tags, users;")
-end
